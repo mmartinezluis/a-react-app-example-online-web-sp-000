fe2b399614e14d1fb65ac2a372f0f2f129325d26
Luis Martinez <luis_mmartinez@live.com>
2021-06-25 15:15:47 -0400

    Done.

---
+++ src/ArticleComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Article extends React.Component {
+  render (){
+    return (
+      <div>
+          Daer Reader: Bjarne Stroustrop has the perfect lecture oration.
+      </div>
+    )
+  }
+}
+
+export default Article;
